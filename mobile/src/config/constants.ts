c0c75b45d74e2f2617ce4d8db5d96d3a9a6e405e
Léo FRANZ <leofranz2006@gmail.com>
2026-03-19 07:28:44 +0100

    chore: update mobile/src/config/constants.ts

--- mobile/src/config/constants.ts
+++ mobile/src/config/constants.ts
@@ -1,2 +1,4 @@
 export const API_BASE_URL = "https://scores.leofranz.fr";
 export const REGISTER_URL = `${API_BASE_URL}/register`;
+export const FORGOT_PASSWORD_URL = `${API_BASE_URL}/forgot-password`;
+export const TERMS_URL = `${API_BASE_URL}/cgu`;
